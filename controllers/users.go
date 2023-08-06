b7e323e4b28009b15fa9f4cec21176ca76abed47
Zaed Abdullah <z4ed.thalib123@gmail.com>
2023-08-07 06:05:49 +0700

    Fix users controller

--- controllers/users.go
+++ controllers/users.go
@@ -1,8 +1,10 @@
 package controllers
 
 import (
+	"encoding/json"
 	"fmt"
 	"go-bookshelf/models"
+	"log"
 	"net/http"
 
 	"github.com/gorilla/mux"
@@ -21,26 +23,33 @@ func Register(w http.ResponseWriter, r *http.Request) {
 	r.ParseForm()
 
 	var user = []models.User{
-		{Name: r.Form["name"][0], Email:r.Form["email"][0], Password:r.Form["password"][0]},
+		{Name: r.Form["name"][0], Email: r.Form["email"][0], Password: r.Form["password"][0]},
 	}
 	models.Db.Create(&user[0])
 	fmt.Fprint(w, "Your account successfully registered")
 }
 
 func Login(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(http.StatusOK)
-	w.Header().Set("Content-Type", "application/json")
 	r.ParseForm()
 	var user []models.User
 	emailCheckErr := models.Db.Where("email = ?", r.Form["email"][0]).First(&user).Error
 	pwdCheckErr := models.Db.Where("password = ?", r.Form["password"][0]).First(&user).Error
 
-	fmt.Printf("emailCheck: %v", emailCheckErr)
-	fmt.Printf("pwdCheck: %v", pwdCheckErr)
+	type Response struct {
+		Message string `json:"message"`
+	}
+	if emailCheckErr == nil && pwdCheckErr == nil {
+		var res Response
+		res.Message = "Your account successfully logged in"
 
-	if emailCheckErr==nil&&pwdCheckErr==nil{
-		fmt.Fprint(w, "Your account successfully logged in")
-	} else{
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+
+		err := json.NewEncoder(w).Encode(res)
+		if err != nil {
+			log.Fatalln(err)
+		}
+	} else {
 		fmt.Fprint(w, "Email or password that you're inputted is wrong")
 	}
 }
