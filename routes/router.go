cdd912e47cf973ee436555c68bf415ca3e3ed777
Zaed Abdullah <z4ed.thalib123@gmail.com>
2024-09-28 17:58:03 +0700

    Delete unneeded route. Add /books subrouter. Use middleware VerifyJwtToken for all /books subrouter

--- routes/router.go
+++ routes/router.go
@@ -1,27 +1,30 @@
 package routes
 
 import (
-	"github.com/gorilla/mux"
-	"net/http"
-	"log"
 	"go-bookshelf/controllers"
-	"github.com/rs/cors"
 	"go-bookshelf/models"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	"github.com/rs/cors"
+	"go-bookshelf/middleware"
 )
 
 func Routes() {
 	models.Db_connection()
 	r := mux.NewRouter()
 	//users
-	r.HandleFunc("/hello/{name}", controllers.Hello).Methods("GET")
 	r.HandleFunc("/register", controllers.Register).Methods("POST")
 	r.HandleFunc("/login", controllers.Login).Methods("POST")
 	//books
-	r.HandleFunc("/books", controllers.GetAllBooks).Methods("GET")
-	r.HandleFunc("/books/{id}", controllers.GetBookById).Methods("GET")
-	r.HandleFunc("/books", controllers.AddBook).Methods("POST")
-	r.HandleFunc("/books/{id}", controllers.UpdateBookById).Methods("PUT")
-	r.HandleFunc("/books/{id}", controllers.DeleteBookById).Methods("DELETE")
+	books := r.PathPrefix("/books").Subrouter()
+	books.Use(middleware.VerifyJwtToken)
+	books.HandleFunc("", controllers.GetAllBooks).Methods("GET")
+	books.HandleFunc("/{id}", controllers.GetBookById).Methods("GET")
+	books.HandleFunc("", controllers.AddBook).Methods("POST")
+	books.HandleFunc("/{id}", controllers.UpdateBookById).Methods("PUT")
+	books.HandleFunc("/{id}", controllers.DeleteBookById).Methods("DELETE")
 	
 	handler := cors.Default().Handler(r)
 	log.Fatal(http.ListenAndServe(":8080", handler))
