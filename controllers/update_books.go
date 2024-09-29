2a736cf160919704759e42df7dbec72fbdcfdcfe
Zaed Abdullah <z4ed.thalib123@gmail.com>
2024-09-29 21:23:39 +0700

    Import time package. Add ResponseBook and ResponseData struct. Change the data type of Data property to ResponseData in Response struct. Get the original CreatedAt value of the book. Then, insert that value into the book CreatedAt property. Create a resBook object and fill in the properties using the book data. Create a resData object and fill the Book property with the resBook object. Fill res.Data value with resData object.

--- controllers/update_books.go
+++ controllers/update_books.go
@@ -7,12 +7,14 @@ import (
 	"net/http"
 	"github.com/gorilla/mux"
 	"strconv"
+	"time"
 )
 
 func UpdateBookById(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json; charset=utf-8")
 	r.ParseForm()
 	uri_param := mux.Vars(r)
+	bookId_int, err := strconv.Atoi(uri_param["id"])
 	type RequestData struct {
 		Name      string `json:"name"`
 		Year      int    `json:"year"`
@@ -24,11 +26,29 @@ func UpdateBookById(w http.ResponseWriter, r *http.Request) {
 		Reading   bool    `json:"reading"`
 	}
 
+	type ResponseBook struct {
+		Id 				uint	 `json:"id"`
+		Name      string `json:"name"`
+		Year      int    `json:"year"`
+		Author    string `json:"author"`
+		Summary   string `json:"summary"`
+		Publisher string `json:"publisher"`
+		PageCount int    `json:"pageCount"`
+		ReadPage  int    `json:"readPage"`
+		Finished  bool   `json:"finished"`
+		Reading   bool    `json:"reading"`
+		CreatedAt  time.Time    `json:"insertedAt"`
+		UpdatedAt   time.Time   `json:"updatedAt"`
+	}
+	type ResponseData struct {
+		Book			ResponseBook	`json:"book"`
+	}
+
 	var reqData RequestData
 	type Response struct {
-		Status  string      `json:"status"`
-		Message string      `json:"message"`
-		Data    RequestData `json:"data"`
+		Status  string       `json:"status"`
+		Message string       `json:"message"`
+		Data    ResponseData `json:"data"`
 	}
 	var res Response
 
@@ -44,11 +64,34 @@ func UpdateBookById(w http.ResponseWriter, r *http.Request) {
 		ReadPage:  reqData.ReadPage,
 		Reading:   reqData.Reading,
 	}
-	bookId_int, err := strconv.Atoi(uri_param["id"])
+	if(err!=nil){
+		fmt.Println("Error: ", err)
+	}
+	var oriBook models.Book
+	models.Db.First(&oriBook, uint(bookId_int))
+	book.CreatedAt = oriBook.CreatedAt
+
 	book.ID = uint(bookId_int)
 	result := models.Db.Save(&book)
 	err = result.Error
 
+	var resBook = ResponseBook{
+		Id: book.ID,
+		Name: book.Name,
+		Year: book.Year,
+		Author: book.Author,
+		Summary: book.Summary,
+		Publisher: book.Publisher,
+		PageCount: book.PageCount,
+		ReadPage: book.ReadPage,
+		Finished: book.Finished,
+		Reading: book.Reading,
+		CreatedAt: book.CreatedAt,
+		UpdatedAt: book.UpdatedAt,
+	}
+	var resData = ResponseData{
+		Book: resBook,
+	}
 	if err != nil {
 		w.WriteHeader(http.StatusBadRequest)
 		fmt.Fprint(w, err)
@@ -58,7 +101,7 @@ func UpdateBookById(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusOK)
 		res.Status = "success"
 		res.Message = "Buku berhasil diperbarui"
-		res.Data = reqData
+		res.Data = resData
 
 		json.NewEncoder(w).Encode(res)
 	}
